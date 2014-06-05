e0878c6bc3a18505dc530b5647c03370a8bb135e
daichi <prinum101@gmail.com>
2014-06-05 15:53:05 +0900

    alertを出す

---
+++ Readme.md
@@ -0,0 +1,23 @@
+#swiftを試す
+
+##参考サイト
+
+###[The Swift Programming Language - Function（関数）をまとめる](http://qiita.com/kiyotaman/items/45a62bd8502df8e872ae)
+
+関数の定義
+
+###[Swiftに関するメモ](http://qiita.com/takkyun/items/080a81a43111387c01fd)
+
+制御文
+
+###[The Swift Programming Language - Strings and Characters（文字列型、文字型）をまとめる](http://qiita.com/kiyotaman/items/323967e0b52fc4fdc6a0)
+
+文字列
+
+###[\[Swift\] Swiftのoptional valueの便利さ /「?」と「!」でより堅牢なコードへ](http://qiita.com/nori0620/items/afa94cc42f3233f54bdc)
+
+`String?`とかのお話
+
+###[How would I create a UIAlertView in Swift?](http://stackoverflow.com/questions/24022479/how-would-i-create-a-uialertview-in-swift)
+
+UIAlertViewの新しい使い方
--- swift-practice/ViewController.swift
+++ swift-practice/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var btn : UIButton
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,15 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func touch(sender : AnyObject) {
+        NSLog("touch")
+        //http://stackoverflow.com/questions/24022479/how-would-i-create-a-uialertview-in-swift
+        var alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+//        self.presentViewController(alert, animated: true, completion: nil)
+        self.presentViewController(alert, animated: true, completion: {() -> Void in
+            NSLog("alert completion" )
+        })
+    }
 }
 
